46c1c2e4fd8fdc8a114310287932d69fe6163d71
chencl <chencl@spreadst.com>
2018-01-31 12:49:38 +0800

    指示器与viewpager结合

--- app/src/main/java/com/musicplayer/ccl/music_player/ui/activity/MainActivity.java
+++ app/src/main/java/com/musicplayer/ccl/music_player/ui/activity/MainActivity.java
@@ -4,20 +4,31 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.musicplayer.ccl.music_player.R;
 import com.musicplayer.ccl.music_player.adapter.MainPagerAdapter;
 import com.musicplayer.ccl.music_player.ui.fragment.MusicListFragment;
 import com.musicplayer.ccl.music_player.ui.fragment.AudioListFragment;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import utils.LogUtils;
 
 public class MainActivity extends BaseActivity {
 
     private ViewPager viewPager;
     private MainPagerAdapter mAdapter;
     private List<Fragment> mFragments;
+    private TextView tv_video;
+    private TextView tv_audio;
+    private View mainIndicateLine;
+    private int screenWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         viewPager = (ViewPager) findViewById(R.id.mian_viewpager);
+        tv_video = (TextView) findViewById(R.id.mian_tv_video);
+        tv_audio = (TextView) findViewById(R.id.mian_tv_audio);
+        mainIndicateLine = findViewById(R.id.main_indicate_line);
 
     }
 
@@ -40,6 +54,9 @@ public class MainActivity extends BaseActivity {
         mFragments = new ArrayList<>();
         mAdapter = new MainPagerAdapter(getSupportFragmentManager(), mFragments);
         viewPager.setAdapter(mAdapter);
+        viewPager.setOnPageChangeListener(new OnVideoPageChangeListener());
+        tv_video.setOnClickListener(this);
+        tv_audio.setOnClickListener(this);
     }
 
     @Override
@@ -47,11 +64,76 @@ public class MainActivity extends BaseActivity {
         mFragments.add(new AudioListFragment());
         mFragments.add(new MusicListFragment());
         mAdapter.notifyDataSetChanged();
+        updateTabs(0);
+
+        //初始化指示器的屏幕宽度的一半
+
+        screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+        mainIndicateLine.getLayoutParams().width = screenWidth /2;
+//        mainIndicateLine.invalidate();
+        mainIndicateLine.requestLayout();
+
+
+
 
     }
 
     @Override
     protected void processClick(View view) {
 
+    }
+
+    /**
+     * 当viewpager发生变化的时候会回调此方法
+     *
+     * */
+    private class OnVideoPageChangeListener implements ViewPager.OnPageChangeListener {
+        /**当page滑动的时候回调此方法*/
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//            LogUtils.e(getClass(),"position " +position+ "   positionOffset  " +positionOffset + "   positionOffsetPixels " +positionOffsetPixels) ;
+                //移动距离 = 起始位置 + 偏移大小
+                // 起始位置 = posion  * 指示器宽度
+                // 偏移大小 = 手指划过屏幕的百分百 * 指示器宽度
+                int offsetX = (int) (positionOffset * mainIndicateLine.getWidth());
+                int startX = position * mainIndicateLine.getWidth();
+                int translationX = startX + offsetX;
+
+            ViewHelper.setTranslationX(mainIndicateLine,translationX);
+
+        }
+        /**当page被选中的时候回调此方法*/
+        @Override
+        public void onPageSelected(int position) {
+            updateTabs(position);
+
+        }
+
+
+        /**当pager的滑动发生变化时候调用*/
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+
+    private void updateTabs(int position) {
+        updateTab(position, 0, tv_video);
+        updateTab(position, 1, tv_audio);
+    }
+
+
+    /**根据tab是否等于position 修改颜色和大小*/
+    private void updateTab(int position, int tabposion, TextView tab) {
+        int green = getResources().getColor(R.color.green);
+        int halfwhite = getResources().getColor(R.color.halfwhite);
+        if (position == tabposion) {
+            tab.setTextColor(green);
+            ViewPropertyAnimator.animate(tab).scaleX(1.2f).scaleY(1.2f);
+            //tab.setTextSize();
+        } else {
+            tab.setTextColor(halfwhite);
+            ViewPropertyAnimator.animate(tab).scaleX(1.0f).scaleY(1.0f);
+        }
     }
 }
